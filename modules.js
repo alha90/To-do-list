78ec45a63bf7040862d521afae07559ef9b5848a
alha90 <hab.tourist@gmail.com>
2024-03-11 00:45:13 -0500

    introduced a module file to resuse some functions, added icons for delete and edit

---
+++ modules.js
@@ -0,0 +1,17 @@
+/* 
+The module below gets a specific key entry (in our case an array) from localStorage
+by passing in the key name
+
+*/
+export function getElements(data) {
+  let arr = [];
+  if (
+    localStorage.length != 0 &&
+    Array.isArray(JSON.parse(localStorage.getItem(data)))
+  ) {
+    JSON.parse(localStorage.getItem(data)).forEach((e) => {
+      arr.push(e);
+    });
+  }
+  return arr;
+}
