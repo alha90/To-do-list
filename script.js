365baa25a829470a40ba16652e1c5642a79d2f3b
alha90 <hab.tourist@gmail.com>
2024-03-11 00:44:37 -0500

    introduced a module file to resuse some functions, added icons for delete and edit

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
-const form =document.querySelector('form');
-// console.log(form)
-const input = document.querySelector('form input')
+import { getElements } from "./modules.js";
 
-const ul = document.querySelector('.task-list-pending');
-form.addEventListener('submit', e => {
-    const li = document.createElement('li');
-    e.preventDefault()
-    let arr = [];
-    // arr.push(input.value);
-    if (localStorage.length != 0) {
-        arr.push(input.value)
-    } else { arr = getElements().push(input.value)}
-    localStorage.setItem('tasks', JSON.stringify(arr));
-    li.appendChild(document.createTextNode(input.value));
-    li.classList.add('li-styling')
-    ul.appendChild(li)
-    form.reset()
-})
+const form = document.querySelector("form");
+const input = document.querySelector("form input");
 
-function loadElements () {
-    getElements().forEach(e => {
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(e));
-        li.classList.add('li-styling')
-        ul.appendChild(li)
-    })
-    
+const ul = document.querySelector(".task-list-pending");
+form.addEventListener("submit", (e) => {
+  const li = document.createElement("li");
+  const button = document.createElement("button");
+  const button1 = document.createElement("button");
+  e.preventDefault();
+  let arr;
+  let str = input.value.charAt(0).toUpperCase() + input.value.slice(1);
+  if (localStorage.length != 0 && getElements("tasks")) {
+    arr = getElements("tasks");
+    arr.push(str);
+  } else {
+    arr.push(str);
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(arr));
+  button.setAttribute("id", "delete");
+  button1.setAttribute("id", "edit");
+  li.append(document.createTextNode(str), button1, button);
+  li.classList.add("li-styling");
+  ul.append(li);
+  form.reset();
+});
+
+function loadElements() {
+  getElements("tasks").forEach((e) => {
+    const li = document.createElement("li");
+    const object = document.createElement("object");
+    const button = document.createElement("button");
+    const button1 = document.createElement("button");
+    button.setAttribute("id", "delete");
+    button1.setAttribute("id", "edit");
+    li.append(document.createTextNode(e), button1, button);
+    li.classList.add("li-styling");
+    ul.append(li);
+  });
 }
 
-function getElements () {
-    let arr = []
-    if (localStorage.length != 0) {
-        JSON.parse(localStorage.getItem('tasks')).forEach(e => {
-            arr.push(e)
-        });
-    }
-    return arr;
-}
-window.addEventListener('load', e => {
-    loadElements()
-})
+window.addEventListener("load", (e) => {
+  loadElements();
+});
+setTimeout(function () {
+  const taskElement = document.querySelectorAll(".task-list-pending li button");
+  console.log(taskElement);
+  taskElement.forEach((e) => {
+    e.addEventListener("click", (event) => {
+      //   console.log(event.target.id);
+      if (event.target.id == "edit") {
+        console.log(event.target.id);
+      }
+    });
+  });
+}, 1000);
+
+// taskElement.addEventListener("click", (e) => {
+//   console.log(e);
+// });
